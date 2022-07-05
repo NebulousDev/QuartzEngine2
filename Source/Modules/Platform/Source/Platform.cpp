f2180fd8b4aed730fc7a4c83d5b73ad6ccfde01a
NebulousDev <nebulousdev@gmail.com>
2022-07-05 14:24:44 -0400

    Added platform module

---
+++ Source/Modules/Platform/Source/Platform.cpp
@@ -0,0 +1,23 @@
+#include "System/System.h"
+#include "Quartz.h"
+
+extern "C"
+{
+	bool QUARTZ_API SystemQuery(bool isEditor, Quartz::SystemQueryInfo& systemQuery)
+	{
+		systemQuery.name = "PlatformModule";
+		systemQuery.version = "1.0.0";
+
+		return true;
+	}
+
+	bool QUARTZ_API SystemLoad()
+	{
+		return true;
+	}
+
+	void QUARTZ_API SystemUnload()
+	{
+
+	}
+}
