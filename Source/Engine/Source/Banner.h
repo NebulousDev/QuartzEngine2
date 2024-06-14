36c8e2c748835a71e768cd781dd7cf25bdfb4ffe
NebulousDev <nebulousdev@gmail.com>
2024-06-14 10:32:02 -0400

    Update log splash copyright

--- Source/Engine/Source/Banner.h
+++ Source/Engine/Source/Banner.h
@@ -11,6 +11,6 @@ namespace Quartz
 		LogRaw(LOG_CYAN u8" |                    Nebulous Games                   | \n");
 		LogRaw(LOG_CYAN u8" |                 QUARTZ ENGINE v2.0.0                | \n");
 		LogRaw(LOG_CYAN u8" ------------------------------------------------------- \n");
-		LogRaw(LOG_CYAN u8"  ~ Copyright \u00A9 Ben Ratcliff (NebulousDev) 2019-2022 ~   \n\n");
+		LogRaw(LOG_CYAN u8"  ~ Copyright \u00A9 Ben Ratcliff (NebulousDev) 2019-2024 ~   \n\n");
 	}
 }
